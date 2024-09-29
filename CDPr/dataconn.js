15dd9ce69b38cc8dc1a2b781dfe28c59e87db251
Samyak-Kalaskar <145278506+Samyak-Kalaskar@users.noreply.github.com>
2024-09-30 00:20:16 +0530

    Update dataconn.js

--- CDPr/dataconn.js
+++ CDPr/dataconn.js
@@ -1,11 +1,15 @@
 var mysql = require('mysql');
 
+// Create a connection to the database
 var conn = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123456"
+    password: "",  // Leave empty if there's no password
+    database: "sam"
 });
 
+
+// Connect to the database
 conn.connect(function(err) {
     if (err) {
         throw err;
