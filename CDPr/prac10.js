dfa69e6075da5e1695ecb254265f99d881029264
Samyak-Kalaskar <145278506+Samyak-Kalaskar@users.noreply.github.com>
2024-09-30 23:25:15 +0530

    Create prac10.js

---
+++ CDPr/prac10.js
@@ -0,0 +1,23 @@
+var mysql=require('mysql');
+var conn=mysql.createConnection({
+host:"localhost",
+user:"root",
+password:"",
+database:"sam"
+});
+conn.connect(function(err){
+if(err)
+throw err;
+console.log("Database is Connected!");
+var sql= "INSERT INTO employee (name, address) VALUES ?";
+var VALUES =[ ['Dhoni','Ranchi'],
+['Virat','Delhi'],
+['Shreyash','Mumbai'],
+['Rohit','Mumbai']
+];
+conn.query(sql,[VALUES],function(err,result){
+if(err)
+throw err;
+console.log("Number of Records are inserted");
+});
+});
